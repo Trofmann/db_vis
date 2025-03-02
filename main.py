00e5afed756545c9f3b45fddf68b897fd0dfa374
Evgeniy <trofimovevgeniy9@gmail.com>
2025-03-02 16:17:49 +0300

    Интерфейс

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import uvicorn
 from fastapi import FastAPI
-from entities import DbData
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
 from db_data_extractor import DbDataExtractor
+from entities import DbData
+
+app = FastAPI()
+
+# Разрешаем CORS для всех запросов
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем все домены
+    allow_methods=["*"],  # Разрешаем все HTTP-методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
 
 app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return FileResponse('interface/interface.html')
+
+
 @app.get(
-    '/get_data/',
+    '/get_data',
     response_model=DbData,
 )
 def get_data() -> DbData:
     return DbDataExtractor()()
+
+
+uvicorn.run(app)
